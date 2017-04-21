3ee97079433552d47ed4984c65736330a40b99f7
Gustavo_Otazu <gustavootazu2010@hotmail.com>
2017-04-21 10:47:02 -0400

    Comentarios de recursos y tipos de recursos

--- recursos/models.py
+++ recursos/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 class TipoDeRecurso(models.Model):
+    """ Models que implementa tipos de recursos para la asignacion en recursos"""
     nombre = models.CharField(max_length=100)
 
     def __unicode__(self):
@@ -9,6 +10,7 @@ class TipoDeRecurso(models.Model):
 
 
 class Recurso(models.Model):
+    """Models que implementa Recursos creacion de recursos"""
     codigo = models.CharField(max_length=100)
     nombre = models.CharField(max_length=100)
     tipo = models.ForeignKey("TipoDeRecurso")

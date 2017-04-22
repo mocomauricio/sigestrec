cd6329922325de5aad5512f39edf28bc9303a6be
Gustavo_Otazu <gustavootazu2010@hotmail.com>
2017-04-22 09:27:13 -0400

    test unitarios de recursos y usuarios

--- recursos/tests.py
+++ recursos/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+from django.test import TestCase
+from recursos.models import Recurso
+
+class RecursoTestCase(TestCase):
+    def setUp(self):
+        Recurso.objects.create(codigo=12334, nombre="proyector",tipo= "", observaciones="")
+        Recurso.objects.create(codigo=12335, nombre="Aula", tipo="", observaciones="")
+    def test_recurso(self):
+        """Recursos """
+        proyector = Recurso.objects.get(name="proyector")
+        Aula= Recurso.objects.get(name="Aula")
